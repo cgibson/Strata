c6ac23e6507470f9cee70f513e07d5f0a39f3235
Chris Gibson <cgibson@Chris-Gibsons-Mac-Pro.local>
2012-01-05 23:20:15 -0800

    added model view test project and basis files for loading and storing object files

---
+++ ModelViewTest/ModelViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GLESContextTest
+//
+//  Created by Chris Gibson on 12/18/11.
+//  Copyright (c) 2011 PDI/DreamWorks Animation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ModelViewTest/ModelViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ModelViewTest
+//
+//  Created by Chris Gibson on 1/5/12.
+//  Copyright (c) 2012 PDI/DreamWorks Animation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
